3d522ce1bd7c2aa2af9fd06490eb6615927a513e
문현준 <jann1653@gmail.com>
2024-07-04 21:34:46 +0900

    인프런 Readable-Code 강의 섹션4 도메인 지식 얻기 완료

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,20 +1,20 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
-import cleancode.minesweeper.tobe.io.InputHandler;
-import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Middle();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
+        GameConfig gameConfig = new GameConfig(
+            new Middle(),
+            new ConsoleInputHandler(),
+            new ConsoleOutputHandler()
+        );
 
-        MineSweeper mineSweeper = new MineSweeper(gameLevel, inputHandler, outputHandler);
+        MineSweeper mineSweeper = new MineSweeper(gameConfig);
         mineSweeper.initialize();
         mineSweeper.run();
     }
